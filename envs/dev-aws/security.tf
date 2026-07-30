8c64abe5d25d04e728cb872e9150fc8e53873b9a
Anne-LaureS <AnneLaureSimonnet@proton.me>
2026-07-30 16:46:12 +0200

    fix: ouvre l'egress HTTP (80) en plus du HTTPS (443), nécessaire pour apt-get

--- envs/dev-aws/security.tf
+++ envs/dev-aws/security.tf
@@ -29,6 +29,12 @@ resource "aws_security_group" "web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "tp2-web-sg"
   }
